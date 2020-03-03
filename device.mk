27998182e6ed5ce0f70fa55309a63e3d635862c8
Bowgo Tsai <bowgotsai@google.com>
2020-03-03 12:15:50 +0800

    pissarro: Removing GSI keys
    Those keys will be embedded into VtsSecurityAvb on host side
    instead, to verify the GSI image used on the device.
    
    Bug: 149806769
    Test: build and checks those keys are removed from
          $OUT/recovery/root/first_stage_ramdisk/avb/
    Change-Id: I8a002ba6f1421fb460056ccae6572050bdb0ce3c

--- device.mk
+++ device.mk
@@ -16,9 +16,6 @@
 
 DEVICE_PATH := device/xiaomi/pissarro
 
-# Installs gsi keys into ramdisk, to boot a GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
-
 $(call inherit-product, vendor/xiaomi/pissarro/pissarro-vendor.mk)
 
 PRODUCT_SHIPPING_API_LEVEL := 30
