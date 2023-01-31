8d1243346f4563e9574c6a48467d4bef137eaf56
Kartik-91 <kartikbanwalaop@gmail.com>
2023-01-31 11:54:00 +0530

    pissarro: upate blobs list to V14.0.23.1.3.DEV
    Signed-off-by: Kartik-91 <kartikbanwalaop@gmail.com>

--- device.mk
+++ device.mk
@@ -186,7 +186,8 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.1.vendor \
     android.hardware.drm@1.2.vendor \
     android.hardware.drm@1.3.vendor \
-    android.hardware.drm@1.4.vendor
+    android.hardware.drm@1.4.vendor \
+    liboemcrypto.vendor
 
 # Dumpstate
 PRODUCT_PACKAGES += \
